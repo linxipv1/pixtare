facd685be9dcde01a7fda7e9fa8df8ef8cb7dd25
Linxip <ecrin@linxip.com>
2025-10-07 17:41:04 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    'text-blue-600',
+    'text-green-600',
+    'text-purple-600',
+    'text-orange-600',
+    'bg-blue-50',
+    'bg-green-50',
+    'bg-purple-50',
+    'bg-orange-50',
+    'border-blue-200',
+    'border-green-200',
+    'border-purple-200',
+    'border-orange-200',
+  ],
   theme: {
     extend: {},
   },
